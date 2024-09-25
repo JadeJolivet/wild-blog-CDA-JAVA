235ff54243bac77608562390d2b7423fd06406a9
Jade Jolivet <jade.jolivet@gmail.com>
2024-09-25 16:45:35 +0200

    First Article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ArticleComponentComponent } from './article-component/article-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    ArticleComponentComponent
+  ],
+  templateUrl:'./app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Bienvenue sur le Wild Blog de Jade';
